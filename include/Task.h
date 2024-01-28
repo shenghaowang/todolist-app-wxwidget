177005b5c10ed17eaad2b3fff7760b93850ca72f
Shenghao1993 <wangshenghao1993@gmail.com>
2024-01-28 19:33:03 +0800

    Add functions for loading and savings tasks

---
+++ include/Task.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <vector>
+
+struct Task
+{
+    std::string description;
+    bool done;
+};
+
+void saveTasksToFile(const std::vector<Task>& tasks, const std::string& fileName);
+std::vector<Task> loadTasksFromFile(const std::string& fileName);
---
+++ src/Task.cpp
@@ -0,0 +1,44 @@
+#include "Task.h"
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <algorithm>
+
+void saveTasksToFile(const std::vector<Task>& tasks, const std::string& fileName)
+{
+  std::ofstream ostream(fileName);
+  ostream << tasks.size();
+
+  for (const Task& task : tasks) {
+    std::string description = task.description;
+    std::replace(description.begin(), description.end(), ' ', '_');
+
+    ostream << "\n" << description << ' ' << task.done;
+  }
+}
+
+std::vector<Task> loadTasksFromFile(const std::string& fileName)
+{
+  // check if the given file exists
+  if (!std::filesystem::exists(fileName)) {
+    return std::vector<Task>();
+  }
+
+  std::vector<Task> tasks;
+  std::ifstream istream(fileName);
+
+  int n;
+  istream >> n;
+
+  for (int i = 0; i < n; i++) {
+    std::string description;
+    bool done;
+
+    istream >> description >> done;
+    std::replace(description.begin(), description.end(), '_', ' ');
+    tasks.push_back(Task{ description, done });
+  }
+
+  return tasks;
+}
